739e1bcdc8b2b814708545d76c0f816574ffda14
Cédric <cedric.degrelle-dev@hotmail.com>
2016-12-21 01:07:25 +0100

    Correctif bug vue auth

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -6,14 +6,14 @@
         {{ $event->title }}<br>
         {{ $event->description }}<br>
         {{ $event->start_date }}<br>
-        {{ $event->end_date }}
+        {{ $event->end_date }}<br>
     @endforeach
 
     @if (Auth::guest())
         <li><a href="{{ url('/login') }}">Login</a></li>
         <li><a href="{{ url('/register') }}">Register</a></li>
     @else
-        {{ Auth::user()->name }}: vous êtes bien connecté  !
+        vous êtes bien connecté  !
 
         <a href="">Panel admin</a>
         <a href="{{ url('/logout') }}"
@@ -25,6 +25,8 @@
         <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
             {{ csrf_field() }}
         </form>
+
+        {{ Auth::user() }}
     @endif
 
 @endsection
