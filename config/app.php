36cbbcf2ff7bc46968b14a9b2192e19751b787b1
Cédric <cedric.degrelle-dev@hotmail.com>
2016-12-21 13:27:36 +0100

    Ajout provider laracast/generator

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Laracasts\Generators\GeneratorsServiceProvider::class,
 
         /*
          * Package Service Providers...
